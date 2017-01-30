8a21990187d71cd53c787a87f20e3c8c375ab81e
Alexandre Di Guida <Alex@Alexandres-MacBook-Pro.local>
2017-01-30 19:09:40 +0000

    added paddle movement - fix ball physics - starting to work on the area detection to reset the game plaer can't catch the ball with a paddle

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -8,6 +8,9 @@ public class BallController : MonoBehaviour {
 
 	private Rigidbody2D myRigidBody;
 
+	public GameObject paddle1;
+	public GameObject paddle2;
+
 	// Use this for initialization
 	void Start () {
 		myRigidBody = GetComponent<Rigidbody2D> ();
@@ -18,4 +21,44 @@ public class BallController : MonoBehaviour {
 	void Update () {
 		
 	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log (other);
+		if(other.tag == "GoalZone")
+		{
+			if(transform.position.x < 0)
+			{
+				transform.position = paddle2.transform.position + new Vector3(-1f, 0, 0);
+				myRigidBody.velocity = new Vector2(-startForce, -startForce);
+
+			} else {
+				transform.position = paddle1.transform.position + new Vector3(1f, 0, 0);
+				myRigidBody.velocity = new Vector2(startForce, startForce);
+
+			}
+		}
+	}
+
+	// Triggered when ball enters in the zone
+//	void OnTriggerEnter2D(Collider2D other) {
+//
+//		Debug.Log("OnTriggerEnter2D");
+//
+//		if (other.tag == "GoalZone") {
+//
+//			// Ball is in the area
+//			Debug.Log("Ball is in the area");
+//
+//			if (transform.position.x < 0) {
+//				// Ball enter in the GoalZone on the left reset the position of the ball next to the Paddle2 on the right
+//				transform.position = paddle2.transform.position + new Vector3 (-1f, 0, 0);
+//				myRigidBody.velocity = new Vector2 (-startForce, -startForce);
+//			} else {
+//
+//				transform.position = paddle1.transform.position + new Vector3 (-1f, 0, 0);
+//				myRigidBody.velocity = new Vector2 (startForce, startForce);
+//			}
+//		}
+//	}
 }
---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+
+	public float speed;
+	public float direction; // Paddle direction : 1 going up, -1 going down, 0 going anywhere
+	public float adjustSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Paddle movement
+		if (Input.GetKey (KeyCode.W)) {
+			// Up
+			transform.position = new Vector3 (
+				transform.position.x, 
+				transform.position.y + (speed * Time.deltaTime),
+				transform.position.z);
+
+			direction = 1;
+		} else if (Input.GetKey (KeyCode.S)) {
+			// Down
+			transform.position = new Vector3 (
+				transform.position.x, 
+				transform.position.y - (speed * Time.deltaTime),
+				transform.position.z);
+
+			direction = -1;
+		} else {
+			direction = 0;
+		}
+	}
+
+	// Ball no longer touching a paddle
+	// Used to make the ball speed up and add some velocity when the direction of the ball changes
+	// it adds some difficulty to the game
+	// x velocity will be faster and faster after the end of collision by * 1.1f - it's a slithly tweak
+	void OnCollisionExit2D(Collision2D other) {
+		other.rigidbody.velocity = new Vector2 (other.rigidbody.velocity.x * 1.1f, other.rigidbody.velocity.y + (direction * adjustSpeed));
+		//Debug.Log (other.rigidbody.velocity);
+	}
+}
